1340ff41f8205e6b8fad246a4f983e19b3070091
Daniel Mezzavilla <daniel.mezzavilla@gmail.com>
2019-02-03 12:42:13 -0200

    Atualiação

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO `restjavaee`.`Produto` (`id`, `fabricante`, `modelo`, `tipo`) VALUES ('1', 'Honda', 'Civic', 'Carro');
+INSERT INTO `restjavaee`.`Produto` (`id`, `fabricante`, `modelo`, `tipo`) VALUES ('2', 'Honda', 'CRV', 'Carro');
+INSERT INTO `restjavaee`.`Produto` (`id`, `fabricante`, `modelo`, `tipo`) VALUES ('3', 'Toyota', 'Corolla', 'Carro');
+
